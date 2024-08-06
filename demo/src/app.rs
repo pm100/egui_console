c0961ad3e8f35efcc9cd80bf757fee5e330ef88d
paul moore <paulmoore100@hotmail.com>
2024-08-05 20:23:57 -0700

    clean out dead code

--- demo/src/app.rs
+++ demo/src/app.rs
@@ -12,7 +12,7 @@ pub struct ConsoleDemo {
 
     #[serde(skip)] // This how you opt-out of serialization of a field
     value: f32,
-    //  #[serde(skip)] // This how you opt-out of serialization of a field
+
     console: ConsoleWindow,
 }
 
@@ -78,7 +78,6 @@ impl eframe::App for ConsoleDemo {
             let mut console_response: ConsoleEvent = ConsoleEvent::None;
             egui::Window::new("Console Window")
                 .open(&mut console_window_open)
-                // .vscroll(false)
                 .default_height(500.0)
                 .resizable(true)
                 .show(ctx, |ui| {
@@ -110,31 +109,6 @@ impl eframe::App for ConsoleDemo {
                 self.console.write("clicked");
                 self.console.prompt();
             }
-            // let console_response =
-            // // The central panel the region left after adding TopPanel's and SidePanel's
-            // ui.heading("eframe template");
-
-            // ui.horizontal(|ui| {
-            //     ui.label("Write something: ");
-            //     ui.text_edit_singleline(&mut self.label);
-            // });
-
-            // ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            // if ui.button("Increment").clicked() {
-            //     self.value += 1.0;
-            // }
-
-            // ui.separator();
-
-            // ui.add(egui::github_link_file!(
-            //     "https://github.com/emilk/eframe_template/blob/main/",
-            //     "Source code."
-            // ));
-
-            // ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-            //     powered_by_egui_and_eframe(ui);
-            //     egui::warn_if_debug_build(ui);
-            //});
         });
     }
 }
